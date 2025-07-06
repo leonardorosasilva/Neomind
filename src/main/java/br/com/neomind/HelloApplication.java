c1fc9df1757bf2bdce65ed323a3dae32d2f52829
leonardorosasilva <03466316022@edu.udesc.br>
2025-07-05 22:56:53 -0300

    first commit

---
+++ src/main/java/br/com/neomind/HelloApplication.java
@@ -0,0 +1,9 @@
+package br.com.neomind;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
