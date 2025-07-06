f2cfbed36848db21b8f0f4c28c266e9007fbe3b9
leonardorosasilva <03466316022@edu.udesc.br>
2025-07-06 01:16:53 -0300

    feat(service) add: toda a logica para o fornecedores implementada

---
+++ src/main/java/br/com/neomind/service/FornecedorService.java
@@ -0,0 +1,114 @@
+package br.com.neomind.service;
+
+import br.com.neomind.model.FornecedorModel;
+import br.com.neomind.model.FornecedorRepository;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.transaction.Transactional;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.ws.rs.BadRequestException;
+import org.hibernate.validator.internal.constraintvalidators.hv.br.CNPJValidator;
+
+import javax.xml.validation.Validator;
+import java.util.List;
+import java.util.Set;
+
+@ApplicationScoped
+public class FornecedorService {
+
+    FornecedorRepository fornecedorRepository;
+    Validator validator;
+
+    private void validateFornecedor(FornecedorModel fornecedor, Boolean isUpdate) {
+        Set<ConstraintViolation<FornecedorModel>> violations = validator.validate(fornecedor);
+        if (!violations.isEmpty()) {
+            StringBuilder errors = new StringBuilder("Erro ao validar fornecedor: ");
+            for (ConstraintViolation<FornecedorModel> violation : violations) {
+                errors.append(violation.getMessage());
+            }
+
+            throw new BadRequestException(errors.toString());
+        }
+
+        if(!CnpjValidator.isValid(fornecedor.getCnpj())){
+            throw new BadRequestException("CNPJ invalido");
+        }
+
+        fornecedorRepository.findAll().forEach(e -> {
+            if(isUpdate && e.getId().equals(fornecedor.getId())){
+                return;
+            }
+
+            if(e.getEmail().equals(fornecedor.getEmail())){
+                throw new BadRequestException("Email " + fornecedor.getEmail() + "ja cadastrado" );
+            }
+
+            if(e.getCnpj().equals(fornecedor.getCnpj())){
+                throw new BadRequestException("CNPJ " + e.getCnpj() + "já cadastrado");
+            }
+
+        });
+
+    }
+
+
+    @Transactional
+    public FornecedorRepository create(FornecedorModel fornecedorModel) {
+        validateFornecedor(fornecedorModel, true);
+        fornecedorRepository.save(fornecedorModel);
+        return fornecedorRepository;
+    }
+
+    @Transactional
+    public FornecedorRepository update(FornecedorModel fornecedorModel) {
+        if(fornecedorRepository.findById(fornecedorModel.getId()).isPresent()){
+            validateFornecedor(fornecedorModel, false);
+            fornecedorRepository.save(fornecedorModel);
+            return fornecedorRepository;
+        } else{
+            throw new BadRequestException("Fornecedor não encontrado");
+        }
+    }
+
+    @Transactional
+    public FornecedorRepository delete(FornecedorModel fornecedorModel) {
+        if(fornecedorRepository.findById(fornecedorModel.getId()).isPresent()){
+            fornecedorRepository.delete(fornecedorModel);
+            return fornecedorRepository;
+        } else{
+            throw new BadRequestException("Erro ao excluir o fornecedor");
+        }
+    }
+
+    public List<FornecedorModel> listFornecedoresPaginated(int page, int pageSize) {
+        if (page < 1) {
+            throw new BadRequestException("O número da página deve ser maior ou igual a 1.");
+        }
+        if (pageSize < 1) {
+            throw new BadRequestException("O tamanho da página deve ser maior ou igual a 1.");
+        }
+        return fornecedorRepository.findAllPaginated(page, pageSize);
+    }
+
+    public long countFornecedores() {
+        return fornecedorRepository.countAll();
+    }
+
+
+
+    @Transactional
+    public int importFornecedoresFromJson(List<FornecedorModel> fornecedoresToImport) {
+        int importedCount = 0;
+        for (FornecedorModel fornecedor : fornecedoresToImport) {
+            try {
+                validateFornecedor(fornecedor, false);
+                fornecedorRepository.save(fornecedor);
+                importedCount++;
+            } catch (BadRequestException e) {
+                System.err.println("Erro ao importar fornecedor " + fornecedor.getName() + ": " + e.getMessage());
+            }
+        }
+        return importedCount;
+    }
+}
